da112b33d1bc81485dcf98425477e11e926c3dce
Robson Durval <robson.ferreira.ext@infracommerce.com.br>
2022-05-31 02:17:36 -0300

    adding tag swagger

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -3,27 +3,27 @@ import {
   Controller,
   Get,
   Inject,
-  Logger,
   Post,
   Query,
   ValidationPipe,
 } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { FilterPaginatedUserDto } from './dto/filter.paginated.dto';
 import { GetUserDto } from './dto/get-user.dto';
 import { PaginateUserDto } from './dto/paginated-user.dto';
 import { UserServiceInterface } from './interface/user-service.interface';
 
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
-  private readonly logger = new Logger(UserController.name);
-
   constructor(
     @Inject('UserServiceInterface')
     private readonly userService: UserServiceInterface,
   ) {}
 
   @Get()
+  @ApiOkResponse({ type: PaginateUserDto })
   async findAll(
     @Query() query?: FilterPaginatedUserDto,
   ): Promise<PaginateUserDto> {
@@ -47,6 +47,7 @@ export class UserController {
   }
 
   @Post()
+  @ApiOkResponse({ type: GetUserDto })
   async create(@Body(ValidationPipe) data: CreateUserDto): Promise<GetUserDto> {
     return this.userService.create(data);
   }
